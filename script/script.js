85acc2cb341809cf427512de7c6470757d197f05
cboswel1 <christianboswell86@gmail.com>
2021-04-25 17:53:23 -0600

    checkboxes working with conditions

--- script/script.js
+++ script/script.js
@@ -2,132 +2,74 @@ const generateBtn = document.querySelector("#generate");
 
 let pass= [];
 
-// Function to generate password
 generatePassword = () => {
+  let passLength = document.querySelector('.passlength').value
+  let upper = document.querySelector(".uppercase");
+  let lower = document.querySelector(".lowercase");
+  let num = document.querySelector(".nums"); 
+  let spec = document.querySelector(".special");
 
 	const upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"; 
 	const lowerCase = "abcdefghijklmnopqrstuvwxyz"
 	const numbers = "1234567890"
 	const special = "!@#$%^&*()"
 
-  // stores users choice from possible selection criteria.  
   let response = ""; 
 
-    //User Prompts Section
-
-    // tell user that they must choose between a range of characters. 8 min, 128 max; 
-
-  const passLength = prompt ("All valid passwords have a minimum of 8 character, and a maximum of 128, please choose now." )
-
-    // if user fails to make choice
-  if (!passLength) {
-      alert("You need to enter a value");
-
+  if (upper.checked == true && lower.checked == true && num.checked == true && spec == true) {
+    response = upperCase.concat(lowerCase, numbers, special);
   } 
-  
-   //if user chooses number outside of parameters
-  else if (passLength < 8 || passLength > 128) {
-      alert("You must choose a password between 8 and 128!")
-
-  } 
-  
-  else {
-
-    // Ask user if they wish to have uppercase letters in their password 
-
-      var upperCon = confirm("Would you like to use uppercase characters in your password?"); 
-  
-    // Ask user if they wish to have lowercase letters in their password 
-    
-      var lowerCon = confirm("Would you like to use lowercase characters in your password?"); 
-
-    //Ask user if they wish to have numbers in their password 
-    
-      var numbersCon = confirm("Would you like to use numbers in your password?"); 
-
-    // Ask user if they wish to have special characters in their password 
-
-      var specialCon = confirm("Would you like to use special characters in your password?");
-  }; 
-  
-    // Let user know they must select at least one criteria 
-
-  if (!upperCon && !lowerCon && !numbersCon && !specialCon) {
-      alert("You must choose at least one criteria. Try again");
-  } 
-  // List of all possible user choices 
-
-  // all choices
-  else if (upperCon && lowerCon && numbersCon && specialCon) {
-      response = upperCase.concat(lowerCase, numbers, special);
-  }
-  // 3 choice
-  else if (upperCon && lowerCon && numbersCon) {
+  else if (upper.checked == true && lower.checked == true && num.checked == true) {
       response = upperCase.concat(lowerCase, numbers);
   } 
-  // 3 choice
-  else if (upperCon && lowerCon && specialCon) {
+  else if (upper.checked == true && lower.checked == true && spec.checked == true) {
       response = upperCase.concat(lowerCase, special);
   } 
-  // 3 choice
-  else if (upperCon && specialCon && numbersCon) {
+  else if (upper.checked == true && spec.checked == true && num.checked == true) {
       response = upperCase.concat(special, numbers);
   } 
-  // 3 choice 
-  else if (lowerCon && specialCon && numbersCon) {
+  else if (spec.checked == true && lower.checked == true && num.checked == true) {
      response = lowerCase.concat(special, numbers);
   }
-  // 2 choice 
-  else if (upperCon && specialCon) {
+  else if (upper.checked == true && spec.checked == true ) {
      response = upperCase.concat(special);
   }
-  // 2 choice
-  else if (upperCon && lowerCon) {
+  else if (upper.checked == true && lower.checked == true) {
      response = upperCase.concat(lowerCon);
   }
-  // 2 choice 
-  else if (upperCon && numbersCon) {
+  else if (upper.checked == true && num.checked == true) {
      response = upperCase.concat(numbers);
   }
-  // 2 choice 
-  else if (lowerCon && specialCon) {
+  else if (lower.checked == true && spec.checked == true) {
      response = lowerCase.concat(special);
   }
-  // 2 choice 
-  else if (lowerCon && numbersCon) {
+  else if (lower.checked == true && num.checked == true) {
      response = lowerCase.concat(numbers); 
-
   }
-  // 2 choice 
-  else if (numbersCon && specialCon) {
+  else if (num.checked == true && spec.checked == true) {
      response = numbers.concat(special);
-  }
-  // 1 choice 
-  else if (upperCon) {
+  } 
+  else if (upper.checked === true) {
      response = upperCase;
   }
-  // 1 choice 
-  else if (lowerCon) {
+  else if (lower.checked === true) {
      response = lowerCase;
-  }
-  // 1 choice 
-  else if (numbersCon) {
+  } 
+  else if (num.checked === true) {
      response = numbers;
   };
-
-  // loop that can take in user's choice (response), and randomize selection based on length
+  
   for (var i = 0; i < passLength; i++) {
     var userChoice = response[Math.floor(Math.random() * response.length)];
-    //.push to take userChoice variable outcome and plug into empty pass array
     pass.push(userChoice);
   };
 }
 
-    // Write password to the #password input
 writePassword = () => {
-	const password = generatePassword();
+  const password = generatePassword();
   const passwordText = document.querySelector("#password");
   passwordText.value = pass.join('');
+
   }
 
 generateBtn.addEventListener("click", writePassword);
